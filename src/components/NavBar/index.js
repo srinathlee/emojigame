83767ea710e30cb8c16f9803cb604c383d87616b
srinath5255 <iamsrinath5255@gmail.com>
2023-04-12 16:58:51 +0000

    first commit

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,26 @@
+import './index.css'
+
+const Navbar = props => {
+  const {scores} = props
+  console.log(scores)
+  const {score, maxscore} = scores
+
+  return (
+    <nav className="navbar-bg-container">
+      <div className="logo-bg-container">
+        <img
+          className="logo-img"
+          src="https://assets.ccbp.in/frontend/react-js/game-logo-img.png"
+        />
+        <p className="logo-name">Emoji Game</p>
+      </div>
+
+      <div className="score-bg-container">
+        <p className="score">Score:{score}</p>
+        <p className="score">Top Score:{maxscore}</p>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
